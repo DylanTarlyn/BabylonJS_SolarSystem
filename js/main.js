fed4108b46f1b137abca227488b24315f3dbefe2
Dylan <d.tarlyn@gmail.com>
2021-12-23 18:24:15 -0800

    Setting up basic scene

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 /// <reference path='./vendor/babylon.d.ts' />
 
-console.log('hello world!!!');
+//get canvas
+const canvas = document.getElementById('renderCanvas');
+
+//create BabylonJS engine
+const engine = new BABYLON.Engine(canvas, true);
+
+function createScene(){
+    //create scene
+    const scene = new BABYLON.Scene(engine);
+
+    //create camera
+    const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0,0,0), scene);
+
+    //create light
+    const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0,1,0), scene);
+
+    return scene;
+}
+
+//create scene
+const scene = createScene();
+
+engine.runRenderLoop(()=>{
+    scene.render();
+});
+
