e11dd8e470f7b258d56ade2ad8a843e963c69886
Dylan <d.tarlyn@gmail.com>
2021-12-23 19:52:39 -0800

    Added sun texture, edited lighting, added sunlight

--- js/main.js
+++ js/main.js
@@ -17,6 +17,9 @@ function createCamera(scene){
 
 function createLight(scene){
     const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0,1,0), scene);
+
+    light.intensity = 0.5;
+    light.groundColor = new BABYLON.Color3(0,0,1);
 }
 
 function createSun(scene){
@@ -24,11 +27,38 @@ function createSun(scene){
         segments:16,
         diameter:4
     }, scene);
+
+    const sunMaterial = new BABYLON.StandardMaterial('sunMaterial', scene);
+    sunMaterial.emissiveTexture = new BABYLON.Texture('/assets/images/sun.jpg',scene);
+    sunMaterial.diffuseColor = BABYLON.Color3.Black();
+    sunMaterial.specularColor = BABYLON.Color3.Black();
+
+    sun.material = sunMaterial;
+
+    //sunlight
+    const sunlight = new BABYLON.PointLight('sunlight', BABYLON.Vector3.Zero(), scene)
+    sunlight.intensity = 2;
 }
+
+function createPlanet(scene){
+    const planet = BABYLON.MeshBuilder.CreateSphere('planet',{
+        segments:16,
+        diameter:1
+    }, scene)
+    planet.position.x = 4;
+
+    const planetMaterial = new BABYLON.StandardMaterial('planetMaterial', scene);
+    planetMaterial.diffuseTexture = new BABYLON.Texture('assets/images/sand.png', scene);
+    planetMaterial.specularColor = BABYLON.Color3.Black();
+
+    planet.material = planetMaterial;
+}
+
 
 function createScene(){
     //create scene
     const scene = new BABYLON.Scene(engine);
+    scene.clearColor = BABYLON.Color3.Black();
 
     //create camera
     createCamera();
@@ -38,6 +68,10 @@ function createScene(){
 
     //create sun
     createSun(scene);
+
+    //create planet
+    createPlanet(scene);
+
 
     return scene;
 }
