a49fcabee7cab2602acf73b32d0a6fea4bd1e7aa
Dylan <d.tarlyn@gmail.com>
2021-12-23 18:48:28 -0800

    Creating test objects

--- js/main.js
+++ js/main.js
@@ -11,10 +11,40 @@ function createScene(){
     const scene = new BABYLON.Scene(engine);
 
     //create camera
-    const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0,0,0), scene);
+    const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0,0,-10), scene);
+    camera.attachControl(canvas, true);
 
     //create light
     const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0,1,0), scene);
+
+    //create box
+    const box = BABYLON.MeshBuilder.CreateBox('box', {
+        size: 1
+    }, scene);
+
+    //create sphere
+    const spere = BABYLON.MeshBuilder.CreateSphere('spere', {
+        segments:32,
+        diameter: 2,
+    }, scene);
+    spere.position = new BABYLON.Vector3(3,0,0);
+
+    //create plane
+    const plane = BABYLON.MeshBuilder.CreatePlane('plane', {}, scene);
+    plane.position = new BABYLON.Vector3(-3,0,0);
+
+    //create line
+
+    const points = [
+        new BABYLON.Vector3(2,0,0),
+        new BABYLON.Vector3(2,1,1),
+        new BABYLON.Vector3(2,1,0),
+    ];
+
+    const lines = BABYLON.MeshBuilder.CreateLines('lines', {
+        points
+    }, scene)
+
 
     return scene;
 }
